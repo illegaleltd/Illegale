e4865f6f665ad1f463cf64091086e33848eeb30e
illegaleltd <wayandnj12@gmail.com>
2024-09-18 22:02:41 +0800

    Files upload

---
+++ js/footer.js
@@ -0,0 +1,92 @@
+var footer = 
+`
+    <footer class="footer">
+        <div class="footer-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 mb-30">
+                        <h6 class="color-white mb-20">About Shop</h6>
+                        <p class="color-white body-p2 desc-company">Illegale adalah usaha clothing & apparel yang berdiri pada 11 Juli 2017 yang dimana usaha clothing ini 
+                menjual baju, celana, topi, stiker, tas dan lanyard. Dengan model desain baju yang modern dan menarik.</p>
+                    </div>
+                    <div class="col-lg-8 mb-30">
+                        <div class="row">
+                            <div class="col-lg-4 mb-30">
+                            <h6 class="color-white mb-20">Navigasi</h6>
+                            <ul class="menu-footer">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="produk.html">Products</a></li>
+                                <li><a href="galeri.html">Gallery</a></li>
+                                <li><a href="tentang.html">About Us</a></li>
+                            </ul>
+                            </div>
+                            <div class="col-lg-4 mb-30">
+                            <h6 class="color-white mb-20">Follow Us</h6>
+                            <ul class="menu-footer">
+                                <li><a target="_blank" href="https://www.instagram.com/illegale.ltd/">Instagram</a></li>
+                                <li><a href="https://shopee.co.id/illegale.ltd?v=d41&smtt=0.0.3">Shopee</a></li>
+                            </ul>
+                            </div>
+                            <div class="col-lg-4 mb-30">
+                                <h6 class="color-white mb-20">Contact</h6>
+                                <ul class="menu-footer">
+                                    <li><a href="about-us-1.html">dwinugraha895@gmail.com</a></li>
+                                    <li><a target="_blank" href="https://api.whatsapp.com/send/?phone=6281238757996&text&type=phone_number&app_absent=0">0812 3875 7996</a></li>
+                                    <li><a target="_blank" href="https://maps.app.goo.gl/ZRRUb3w3aWdbHzPY7?g_st=iw">Jl. Janger No. 19 , Dauh Yeh Cani, Kec. Abiansemal</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+`
+
+var footer_produk = 
+`
+    <footer class="footer">
+        <div class="footer-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 mb-30">
+                        <h6 class="color-white mb-20">About Shop</h6>
+                        <p class="color-white body-p2 desc-company">Illegale adalah usaha clothing & apparel yang berdiri pada 11 Juli 2017 yang dimana usaha clothing ini 
+                menjual baju, celana, topi, stiker, tas dan lanyard. Dengan model desain baju yang modern dan menarik.</p>
+                    </div>
+                    <div class="col-lg-8 mb-30">
+                        <div class="row">
+                            <div class="col-lg-4 mb-30">
+                            <h6 class="color-white mb-20">Navigasi</h6>
+                            <ul class="menu-footer">
+                                <li><a href="../index.html">Home</a></li>
+                                <li><a href="../produk.html">Products</a></li>
+                                <li><a href="../galeri.html">Gallery</a></li>
+                                <li><a href="../tentang.html">About Us</a></li>
+                            </ul>
+                            </div>
+                            <div class="col-lg-4 mb-30">
+                            <h6 class="color-white mb-20">Follow Us</h6>
+                            <ul class="menu-footer">
+                                <li><a target="_blank" href="https://www.instagram.com/illegale.ltd/">Instagram</a></li>
+                                <li><a href="https://shopee.co.id/illegale.ltd?v=d41&smtt=0.0.3">Shopee</a></li>
+                            </ul>
+                            </div>
+                            <div class="col-lg-4 mb-30">
+                                <h6 class="color-white mb-20">Contact</h6>
+                                <ul class="menu-footer">
+                                    <li><a href="about-us-1.html">dwinugraha895@gmail.com</a></li>
+                                    <li><a target="_blank" href="https://api.whatsapp.com/send/?phone=6281238757996&text&type=phone_number&app_absent=0">0812 3875 7996</a></li>
+                                    <li><a target="_blank" href="https://maps.app.goo.gl/ZRRUb3w3aWdbHzPY7?g_st=iw">Jl. Janger No. 19 , Dauh Yeh Cani, Kec. Abiansemal</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+`
+
+$("#footer-content").append(footer);
+$("#footer-produk").append(footer_produk);
